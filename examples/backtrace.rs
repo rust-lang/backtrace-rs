5cdf76bbb5d06531a6fa81d67522fc9ff292788c
Andrew DeFilippo <0palescent@protonmail.com>
2019-10-17 11:31:44 -0500

    Added pretty print examples and fixed an out of place comma in the line output.

--- examples/backtrace.rs
+++ examples/backtrace.rs
@@ -5,5 +5,7 @@ use backtrace::Backtrace;
 fn main() {
     let backtrace = Backtrace::new();
     println!("No Precision:\n{:?}\n", backtrace);
-    println!("Precision of 4:\n{:.4?}", backtrace);
+    println!("No Precision Pretty:\n{:#?}\n", backtrace);
+    println!("Precision of 4:\n{:.4?}\n", backtrace);
+    println!("Precision of 4 Pretty:\n{:#.4?}", backtrace);
 }
