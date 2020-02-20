f90563e4795b08e157ef9f5d2d8891b165e2a0f7
Tobias Kortkamp <t@tobik.me>
2020-02-20 13:33:30 +0100

    Add dragonfly too

--- crates/backtrace-sys/build.rs
+++ crates/backtrace-sys/build.rs
@@ -61,7 +61,7 @@ fn main() {
     File::create(out_dir.join("config.h")).unwrap();
     if target.contains("android") {
         maybe_enable_dl_iterate_phdr_android(&mut build);
-    } else if target.contains("freebsd") {
+    } else if target.contains("dragonfly") || target.contains("freebsd") {
         build.define("HAVE_DL_ITERATE_PHDR", "1");
         build.define("HAVE_KERN_PROC", "1");
     } else if target.contains("netbsd") {
