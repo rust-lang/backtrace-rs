9eb9aaa28105b36d23106c83bf96d76751f828b2
leo60228 <leo@60228.dev>
2020-07-20 15:59:43 -0400

    fmt

--- src/symbolize/gimli/mmap_fake.rs
+++ src/symbolize/gimli/mmap_fake.rs
@@ -1,6 +1,6 @@
-use super::{File, mystd::io::Read};
-use core::ops::Deref;
+use super::{mystd::io::Read, File};
 use alloc::vec::Vec;
+use core::ops::Deref;
 
 pub struct Mmap {
     vec: Vec<u8>,
@@ -8,9 +8,7 @@ pub struct Mmap {
 
 impl Mmap {
     pub unsafe fn map(mut file: &File, len: usize) -> Option<Mmap> {
-        let mut mmap = Mmap {
-            vec: Vec::new(),
-        };
+        let mut mmap = Mmap { vec: Vec::new() };
         file.read_to_end(&mut mmap.vec).ok()?;
         mmap.vec.truncate(len);
         Some(mmap)
