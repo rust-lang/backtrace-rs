cff4ef41487eb1b45d5511d87d3db6cbd4e3e7f0
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-07-09 19:22:39 -0700

    test: make the other_thread example noop on other platforms

--- examples/other_thread.rs
+++ examples/other_thread.rs
@@ -1,25 +1,34 @@
-// Windows only currently
+#[cfg(all(windows, not(target_vendor = "uwp")))]
 use backtrace::{Backtrace, BacktraceFrame};
+#[cfg(all(windows, not(target_vendor = "uwp")))]
 use std::os::windows::prelude::AsRawHandle;
 
+#[cfg(all(windows, not(target_vendor = "uwp")))]
 fn worker() {
     foo();
 }
+
+#[cfg(all(windows, not(target_vendor = "uwp")))]
 fn foo() {
     bar()
 }
+
+#[cfg(all(windows, not(target_vendor = "uwp")))]
 fn bar() {
     baz()
 }
+
+#[cfg(all(windows, not(target_vendor = "uwp")))]
 fn baz() {
     println!("Hello from thread!");
     // Sleep for simple sync. Can't read thread that has finished running
-    //std::thread::sleep(std::time::Duration::from_millis(1000));
+    std::thread::sleep(std::time::Duration::from_millis(1000));
     loop {
         print!("");
     }
 }
 
+#[cfg(all(windows, not(target_vendor = "uwp")))]
 fn main() {
     let thread = std::thread::spawn(|| {
         worker();
@@ -40,4 +49,9 @@ fn main() {
     let mut bt = Backtrace::from(frames);
     bt.resolve();
     println!("{:?}", bt);
+}
+
+#[cfg(not(all(windows, not(target_vendor = "uwp"))))]
+fn main() {
+    println!("This example is skipped on non Windows platforms");
 }
