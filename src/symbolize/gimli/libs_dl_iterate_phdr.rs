06ccd70da9f3367fce2842cab5cfa3212d0cbea5
Jubilee Young <workingjubilee@gmail.com>
2023-07-06 17:58:51 -0700

    Use a superpub field in MapsEntry
    This may eliminate some of the overhead of a getter.
    Also use a slightly more modern code style.

--- src/symbolize/gimli/libs_dl_iterate_phdr.rs
+++ src/symbolize/gimli/libs_dl_iterate_phdr.rs
@@ -20,10 +20,9 @@ pub(super) fn native_libraries() -> Vec<Library> {
 fn infer_current_exe(base_addr: usize) -> OsString {
     if let Ok(entries) = super::parse_running_mmaps::parse_maps() {
         let opt_path = entries
-            .iter()
-            .find(|e| e.ip_matches(base_addr) && e.pathname().len() > 0)
-            .map(|e| e.pathname())
-            .cloned();
+            .into_iter()
+            .find(|e| e.ip_matches(base_addr) && !e.pathname.is_empty())
+            .map(|e| e.pathname);
         if let Some(path) = opt_path {
             return path;
         }
