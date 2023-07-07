39ad27673b221c063e352ed974ee87e57c237f78
Jubilee Young <workingjubilee@gmail.com>
2023-07-06 18:13:53 -0700

    Revert "Use a superpub field in MapsEntry"
    This reverts commit 06ccd70da9f3367fce2842cab5cfa3212d0cbea5.

--- src/symbolize/gimli/libs_dl_iterate_phdr.rs
+++ src/symbolize/gimli/libs_dl_iterate_phdr.rs
@@ -36,9 +36,10 @@ fn infer_current_exe(
     #[cfg(not(target_os = "hurd"))]
     if let Some(entries) = maps {
         let opt_path = entries
-            .into_iter()
-            .find(|e| e.ip_matches(base_addr) && !e.pathname.is_empty())
-            .map(|e| e.pathname);
+            .iter()
+            .find(|e| e.ip_matches(base_addr) && e.pathname().len() > 0)
+            .map(|e| e.pathname())
+            .cloned();
         if let Some(path) = opt_path {
             return path;
         }
