aa95934c312e6d3266b50244d1a53f4ce59fba8f
klensy <klensy@users.noreply.github.com>
2024-05-04 13:45:18 +0300

    fix clippy slow_vector_initialization
    Should be smaller codegen size

--- src/dbghelp.rs
+++ src/dbghelp.rs
@@ -409,8 +409,7 @@ pub fn init() -> Result<Init, ()> {
         //
         // See https://learn.microsoft.com/cpp/build/reference/pdbpath for an
         // example of where symbols are usually searched for.
-        let mut search_path_buf = Vec::new();
-        search_path_buf.resize(1024, 0);
+        let mut search_path_buf = vec![0; 1024];
 
         // Prefill the buffer with the current search path.
         if DBGHELP.SymGetSearchPathW().unwrap()(
